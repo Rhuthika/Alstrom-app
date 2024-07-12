c540c2a026b71133714a4242096c4006446d96db
Rhuthika H M <rhhm@dhcp-10-120-134-255.wifi.oracle.com>
2024-07-12 12:37:14 +0530

    Test file chaged

--- src/test/java/com/training/bms/AlstromAppApplicationTests.java
+++ src/test/java/com/training/bms/AlstromAppApplicationTests.java
@@ -1,13 +1,13 @@
 package com.training.bms;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class AlstromAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(190,200);
 	}
 
 }
